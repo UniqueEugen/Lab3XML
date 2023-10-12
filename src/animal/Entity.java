9e96c61c8b041a1e468df472e8e05461d71ba86d
UniqueEugen <112904113+UniqueEugen@users.noreply.github.com>
2023-10-12 23:50:14 +0300

    /

---
+++ src/animal/Entity.java
@@ -0,0 +1,68 @@
+package animal;
+
+import java.util.UUID;
+
+public class Entity {
+    private UUID id;
+    private String type;
+    private double price;
+    private String kind;
+    private String description;
+    private String subspecies;
+
+    public String getType() {
+        return type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getSubspecies() {
+        return subspecies;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setSubspecies(String subspecies) {
+        this.subspecies = subspecies;
+    }
+
+    @Override
+    public String toString() {
+        System.out.println( "id=" + getId()+"\nЖивотное [вид: " + getKind() + ", подвид: " + getSubspecies() +
+                ", семейство: " + getType() + ", цена: " + getPrice() + "]\nОписание: "+getDescription());
+        return null;
+    }
+
+    public String getId() {
+        return id.toString();
+    }
+
+    public void setId(String id) {
+        this.id = UUID.fromString(id);
+    }
+
+}
